d6bd2c7a831fdc814fbb839ba565d9d640e4acbf
WYK <arkwanyuk@gmail.com>
2024-04-03 03:48:05 +0800

    Init the classes

---
+++ js/Annotation.js
@@ -0,0 +1,3 @@
+"strict mode";
+
+class Annotation {}
---
+++ js/Image.js
@@ -0,0 +1,16 @@
+"strict mode";
+
+const DATA_FOLDER = "data"
+class Image {
+    constructor(imageData, annoDatas) {
+        this.jsonData = jsonData;
+        this.id = jsonData.id;
+    }
+
+    getPath() {
+        return `${DATA_FOLDER}/${this.getFileName()}`;
+    }
+
+    getFileName() {
+        return this.jsonData.filename;
+}
